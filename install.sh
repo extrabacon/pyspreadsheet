04b76a811eed45cbf81bc67c46c552b0de9ac838
Nicolas Mercier-Gaboury <nicolas@extrabacon.net>
2013-08-06 10:09:49 -0400

    added dateutil python dependency

--- install.sh
+++ install.sh
@@ -2,26 +2,32 @@ rm -rf deps
 mkdir deps
 cd deps
 
+echo dateutil...
+curl -s -L https://pypi.python.org/packages/source/p/python-dateutil/python-dateutil-2.1.tar.gz#md5=1534bb15cf311f07afaa3aacba1c028b | tar zx
+cd python-dateutil-2.1
+python setup.py install
+cd ..
+
 echo Downloading xlrd dependency from Github...
-rm -rf python/xlrd
+rm -rf ../python/xlrd
 curl -s -L https://github.com/python-excel/xlrd/tarball/master | tar zx
 mv python-excel-xlrd-* python-excel-xlrd
 cp -R python-excel-xlrd/xlrd ../python/xlrd
 
 echo Downloading xlwt dependency from Github...
-rm -rf python/xlwt
+rm -rf ../python/xlwt
 curl -s -L https://github.com/python-excel/xlwt/tarball/master | tar zx
 mv python-excel-xlwt-* python-excel-xlwt
 cp -R python-excel-xlwt/xlwt ../python/xlwt
 
 echo Downloading XlsxWriter dependency from Github...
-rm -rf python/xlsxwriter
+rm -rf ../python/xlsxwriter
 curl -s -L https://github.com/jmcnamara/XlsxWriter/tarball/master | tar zx
 mv jmcnamara-XlsxWriter-* XlsxWriter
 cp -R XlsxWriter/xlsxwriter ../python/xlsxwriter
 
 echo Downloading openpyxl dependency from Bitbucket...
-rm -rf python/openpyxl
+rm -rf ../python/openpyxl
 curl -s -L https://bitbucket.org/ericgazoni/openpyxl/get/default.tar.gz | tar zx
 mv ericgazoni-openpyxl-* ericgazoni-openpyxl
-cp -R ericgazoni-openpyxl ../python/openpyxl
+cp -R ericgazoni-openpyxl/openpyxl ../python/openpyxl
