aa1063af520a609e25b0208c9c3ee61ba585c726
Nicolas Mercier-Gaboury <nicolas@extrabacon.net>
2014-03-07 16:04:40 -0500

    improved installation script

--- install.sh
+++ install.sh
@@ -1,29 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-rm -rf deps
-mkdir deps
-cd deps
+rm -rf .deps
+mkdir .deps
+cd .deps
 
-echo Downloading xlrd dependency from Github...
+echo Downloading xlrd dependency from Github
+URL=https://github.com/python-excel/xlrd/tarball/master
+(wget -O - $URL || curl -s -L $URL) | tar zx
 rm -rf ../python/xlrd
-curl -s -L https://github.com/python-excel/xlrd/tarball/master | tar zx
-mv python-excel-xlrd-* python-excel-xlrd
-cp -R python-excel-xlrd/xlrd ../python/xlrd
+cp -R python-excel-xlrd-*/xlrd ../python/xlrd
 
-echo Downloading xlwt dependency from Github...
+echo Downloading xlwt dependency from Github
+URL=https://github.com/python-excel/xlwt/tarball/master
+(wget -O - $URL || curl -s -L $URL) | tar zx
 rm -rf ../python/xlwt
-curl -s -L https://github.com/python-excel/xlwt/tarball/master | tar zx
-mv python-excel-xlwt-* python-excel-xlwt
-cp -R python-excel-xlwt/xlwt ../python/xlwt
+cp -R python-excel-xlwt-*/xlwt ../python/xlwt
 
-echo Downloading XlsxWriter dependency from Github...
+echo Downloading XlsxWriter dependency from Github
+URL=https://github.com/jmcnamara/XlsxWriter/tarball/master
+(wget -O - $URL || curl -s -L $URL) | tar zx
 rm -rf ../python/xlsxwriter
-curl -s -L https://github.com/jmcnamara/XlsxWriter/tarball/master | tar zx
-mv jmcnamara-XlsxWriter-* XlsxWriter
-cp -R XlsxWriter/xlsxwriter ../python/xlsxwriter
+cp -R jmcnamara-XlsxWriter-*/xlsxwriter ../python/xlsxwriter
 
-echo Downloading openpyxl dependency from Bitbucket...
-rm -rf ../python/openpyxl
-curl -s -L https://bitbucket.org/ericgazoni/openpyxl/get/default.tar.gz | tar zx
-mv ericgazoni-openpyxl-* ericgazoni-openpyxl
-cp -R ericgazoni-openpyxl/openpyxl ../python/openpyxl
+cd ..
+rm -rf .deps
