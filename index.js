c9722946fdec487232bfb453ce530efb0fd9e3e2
Nicolas Mercier-Gaboury <nicolas@extrabacon.net>
2014-03-14 11:45:43 -0400

    fixed resolving of python scripts

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+var path = require('path');
+var PythonShell = require('python-shell');
+PythonShell.defaultOptions = {
+    scriptPath: path.join(__dirname, 'python')
+};
+
 exports.SpreadsheetReader = require('./lib/spreadsheet-reader');
 exports.SpreadsheetWriter = require('./lib/spreadsheet-writer');
